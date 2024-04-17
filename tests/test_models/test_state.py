27a2cc75be152d97e1fa3102e5eab64940ddd99a
Yassid-tech <yassine.idrissi2-etu@etu.univh2c.ma>
2024-04-17 11:51:40 -0700

    Updates

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,7 +2,6 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
-import os
 
 
 class test_state(test_basemodel):
@@ -17,6 +16,4 @@ class test_state(test_basemodel):
     def test_name3(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        self.assertEqual(type(new.name), str)
